e2eb59633a16da7936facae5280cad7c17c992f6
KyDung <kydung@example.com>
2025-12-20 04:41:20 +0700

    Fix vite config for multi-page app build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/LearnPythonWeb/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        game: resolve(__dirname, "game.html"),
+        course: resolve(__dirname, "course.html"),
+        lesson: resolve(__dirname, "lesson.html"),
+        play: resolve(__dirname, "play.html"),
+        login: resolve(__dirname, "login.html"),
+        profile: resolve(__dirname, "profile.html"),
+      },
+    },
+  },
 });
